422ed138a1919224606f282624510882dc7c2e2e
LaurentiaW <laurentiawehmeyer@gmail.com>
2019-11-13 11:28:05 +0100

    generate new dist file before installing sdk

---
+++ pages/projects/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>this a collection of all the projects</h3>
+    <ProjectPreview :project="project" />
+  </div>
+</template>
+
+<script>
+import ProjectPreview from '@/components/Projects/ProjectPreview.vue'
+export default {
+  components: {
+    ProjectPreview
+  },
+  data () {
+    return {
+      project: {
+        brand: 'Nijhof',
+        img:
+          'https://images.pexels.com/photos/2132610/pexels-photo-2132610.jpeg',
+        type: 'Etalages',
+        skillset: 'Styling',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit voluptatem facilis totam quae reiciendis! Rerum obcaecati quia nam laboriosam nemo.',
+        id: '/projects/1'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
